9d106220c5c6261ff96bf445f200de710940ec17
Amon-Ra <amon.raj@gmail.com>
2018-09-02 16:58:29 +0200

    Added support for kubernetes cluster

--- 1.4/entrypoint.sh
+++ 1.4/entrypoint.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+if [ -n ${TZ} ]; then
+  rm /etc/localtime
+  ln -sf /usr/share/zoneinfo/${TZ}  /etc/localtime
+fi
+
+#Openshift fix
+if [ `id -u` -ge 10000 ]; then
+#install nss_wrapper gettext
+# export USER_ID=$(id -u)
+# export GROUP_ID=$(id -g)
+# envsubst < ${HOME}/passwd.template > /tmp/passwd
+# export LD_PRELOAD=libnss_wrapper.so
+# export NSS_WRAPPER_PASSWD=/tmp/passwd
+# export NSS_WRAPPER_GROUP=/etc/group
+    cat /etc/passwd | sed -e "s/^$NB_USER:/builder:/" > /tmp/passwd
+    echo "$NB_USER:x:`id -u`:`id -g`:,,,:/var/lib/flectra:/bin/false" >> /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm /tmp/passwd
+fi
+
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the flectra process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
